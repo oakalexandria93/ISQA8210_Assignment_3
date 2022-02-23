86c4cb07da1ea72c2bfc52ab273ffcfb77d438a5
oakalexandria93 <aoak@unomaha.edu>
2022-02-22 23:57:58 -0600

    Initial commit for Part 1

---
+++ pet/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Pet, Owner
+
+# Register your models here.
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = (
+    'name', 'owner', 'pet_image', 'birthdate','gender', 'spayed_or_neutered',
+    'species', 'breed', 'care_notes', 'food_notes', 'publish')
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'pet','created')
+    list_filter = ('created', 'updated')
+    search_fields = ('name', 'email', 'body')
---
+++ pet/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+#Create your models here
+class Pet(models.Model):
+    name = models.CharField(max_length=250)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name='travelblog_posts')
+    pet_image = models.URLField()
+    birthdate = models.DateField('Birthdate')
+    gender = models.CharField(max_length=100, default='')
+    spayed_or_neutered = models.CharField(max_length=100, default='')
+    species = models.CharField(max_length=100, default='')
+    breed = models.CharField(max_length=100, default='')
+    care_notes = models.TextField(default='')
+    food_notes = models.TextField(default='')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Owner(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField (blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.body}'
