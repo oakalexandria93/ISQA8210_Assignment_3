28e33813edee100be296ca2f9daddd0097410963
oakalexandria93 <aoak@unomaha.edu>
2022-02-23 18:09:54 -0600

    Add .gitignore

--- pet/models.py
+++ pet/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-#Create your models here
+
+# Create your models here
 class Pet(models.Model):
     name = models.CharField(max_length=250)
-    owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name='travelblog_posts')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pet_posts')
     pet_image = models.URLField()
     birthdate = models.DateField('Birthdate')
     gender = models.CharField(max_length=100, default='')
@@ -21,13 +22,13 @@ class Pet(models.Model):
     def __str__(self):
         return self.name
 
+
 class Owner(models.Model):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    #body = models.TextField (blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    #def __str__(self):
+    # def __str__(self):
     #    return f'Comment by {self.name} on {self.body}'
